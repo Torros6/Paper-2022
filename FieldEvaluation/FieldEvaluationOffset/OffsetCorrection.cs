8fb71d00cb28c154e2f8b62907fac0aa83e1e5a6
Albin Karlsson <albka@kth.se>
2022-06-20 08:46:19 +0200

    Script

---
+++ FieldEvaluation/FieldEvaluationOffset/OffsetCorrection.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VMS.TPS.Common.Model.API;
+
+namespace FieldEvaluationOffsetAlbin
+{
+    public partial class OffsetCorrection : Form
+    {
+        private bool _ok;
+        private Offset _offset;
+        private IonPlanSetup m_plan;
+
+        public Offset Offset
+        {
+            get { return _offset; }
+            set { _offset = value; }
+        }
+        public bool OK
+        {
+            get { return _ok; }
+            set { _ok = value; }
+        }
+
+        public OffsetCorrection(Offset offset, IonPlanSetup plan)
+        {
+            this.m_plan = plan;
+            this._offset = offset;
+            InitializeComponent();
+            InitializeGUI();
+        }
+        private void InitializeGUI()
+        {
+            foreach (IonBeam beam in m_plan.IonBeams)
+                lbFields.Items.Add(beam);
+        }
+        private void CheckOutput()
+        {
+            _ok = false;
+            if (!string.IsNullOrEmpty(txtXoffset.Text) && !string.IsNullOrEmpty(txtYoffset.Text) && !string.IsNullOrEmpty(txtZoffset.Text))
+            {
+                Offset.IsoX = -double.Parse(txtXoffset.Text) * 10;
+                Offset.IsoY = -double.Parse(txtYoffset.Text) * 10;
+                Offset.IsoZ = -double.Parse(txtZoffset.Text) * 10;
+
+                if (Offset.IsoX != 0 || Offset.IsoY != 0 || Offset.IsoZ != 0)
+                    Offset.isIsMoved = true;
+
+                _ok = true;
+            }
+        }
+
+        private void OKbtn_Click(object sender, EventArgs e)
+        {
+            CheckOutput();
+            if (!OK)
+                MessageBox.Show("Någon/några rutor är tomma");
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbFields_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbFields.SelectedIndex != -1)
+            {
+                IonBeam currentBeam = m_plan.IonBeams.FirstOrDefault(beam => beam.Id.Equals(Offset.FieldID));
+                txtXoffset.Text = (-(-(currentBeam.IsocenterPosition.x - m_plan.IonBeams.ElementAt(lbFields.SelectedIndex).IsocenterPosition.x) / 10)).ToString("0.00");
+                txtZoffset.Text = (-((currentBeam.IsocenterPosition.y - m_plan.IonBeams.ElementAt(lbFields.SelectedIndex).IsocenterPosition.y) / 10)).ToString("0.00");
+                txtYoffset.Text = (-(-(currentBeam.IsocenterPosition.z - m_plan.IonBeams.ElementAt(lbFields.SelectedIndex).IsocenterPosition.z) / 10)).ToString("0.00");
+            }
+        }
+    }
+}
---
+++ FieldEvaluation/FieldEvaluationOffset/TbaPopup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FieldEvaluationOffsetAlbin
+{
+    public class TbaPopup
+    {
+        public const int BmClick = 0x00F5;
+        private readonly IntPtr _buttonPtr;
+
+        public ManualResetEvent ResetEvent { get; set; } = new ManualResetEvent(true);
+
+        public TbaPopup(IntPtr pointer, bool outOfLimitsPopup = false)
+        {
+            List<string> messages =
+                WinAPI.GetAllChildrenWindowHandles(pointer, 10).Select(WinAPI.GetWindowCaption).ToList();
+            Instructions = messages.Last();
+            if (outOfLimitsPopup)
+            {
+                //BUTTON SAYS YES
+                _buttonPtr =
+                 WinAPI.GetAllChildrenWindowHandles(pointer, 10)
+                   .FirstOrDefault(b => WinAPI.GetWindowCaption(b).Contains("Yes"));
+            }
+            else
+            {
+                //BUTTON SAYS OK
+                _buttonPtr = WinAPI.GetAllChildrenWindowHandles(pointer, 10)
+                   .FirstOrDefault(b => WinAPI.GetWindowCaption(b).Contains("OK"));
+            }
+        }
+
+        public string Instructions { get; set; }
+
+        public void PressOk()
+        {
+            if (_buttonPtr != IntPtr.Zero)
+            {
+                WinAPI.PostMessage(_buttonPtr, BmClick, 0, 0);
+                WinAPI.PostMessage(_buttonPtr, BmClick, 0, 0);
+                WinAPI.PostMessage(_buttonPtr, BmClick, 0, 0);
+                WinAPI.PostMessage(_buttonPtr, BmClick, 0, 0);
+            }
+        }
+    }
+}
